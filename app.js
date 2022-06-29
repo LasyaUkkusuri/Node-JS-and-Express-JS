b30f4fdd074dfaca50cb799511d848d84deb4e4d
LasyaUkkusuri <99169402+LasyaUkkusuri@users.noreply.github.com>
2022-06-29 22:23:10 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,69 +1,37 @@
-// var nd=require('needle')
-// var url="https://jsonplaceholder.typicode.com/todos/1"//same output will come in browser too
-// nd.get(url,(err,response,body)=>{
-//     if(err)
-//         console.log(err)
-//     else
-//         console.log(body)
-// })
+// Include ./utils/geoloc
+//Include ./utils/weatherinfo
+var geoloc = require('./utils/geoloc')
+var weatherinfo = require('./utils/weatherinfo')
 
-// var nd=require('needle')
-// var url="https://api.themoviedb.org/3/movie/top_rated?api_key=abd69698641f2c75e2ee67d12c755e3a&language=en-US&page=1"//same output will come in browser too
-// nd.get(url,(err,response,body)=>{
-//     if(err)
-//         console.log(err)
-//     else
-//         var results=body.results
-//         for(ele in results)
-//             console.log(results[ele].title)
-// })
-
-// var nd=require('needle')
-// var url="https://api.mapbox.com/geocoding/v5/mapbox.places/San%20Francisco.json?access_token=pk.eyJ1IjoibGFzeWF1a2t1c3VyaSIsImEiOiJjbDNvMG45NWQwaTY2M2RsMjRvejJxdnk0In0.yCxHrxZLpHmrcTXo8d9W4A"
-// nd.get(url,(err,response,body)=>{
-//     if(err)
-//         console.log(err)
-//     else
-//         console.log(JSON.parse(body))//console.log(body)
-// })
-
-// var nd=require('needle')
-// var url="http://api.weatherstack.com/current?access_key=a20b80794c3990071dded8d52aef798f&query=Bhimavaram"
-// nd.get(url,(err,response,body)=>{
-//     if(err)
-//         console.log(err)
-//     else
-//         console.log(body.location.lat)
-//         console.log(body.location.lon)
-//         console.log(body.current.temperature)//console.log(body)
-//         var desc=body.current.weather_descriptions
-//         for(ele in desc)
-//             console.log(desc[ele])
-// })
-
-// var nd=require('needle')
-// for(var i=1;i<=500;i++){
-// var url="https://api.themoviedb.org/3/movie/top_rated?api_key=abd69698641f2c75e2ee67d12c755e3a&language=en-US&page="+i
-// nd.get(url,(err,response,body)=>{
-//     if(err)
-//         console.log(err)
-//     else
-//         var results=body.results
-//         for(ele in results)
-//             console.log(results[ele].title)
-// })
-// }
-
-var nd=require('needle')
-for(var i=1;i<=500;i++){
-var url="https://api.themoviedb.org/3/movie/top_rated?api_key=abd69698641f2c75e2ee67d12c755e3a&language=en-US&page="+i
-nd.get(url,(err,response,body)=>{
-    if(err)
-        console.log(err)
-    else
-        var results=body.results
-        for(ele in results)
-            if(results[ele].original_language=='ta')
-                console.log(results[ele].title)
+/* Understand and test the following code by uncommenting it. You can observe the following:
+  First it will print geolocation of given town. 
+  If the geolocation of the town is found then it will display the current temperature of it.
+*/
+geoloc.geoloc("Chennai",function(error,locjson){
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log("Chennai Longitude:"+locjson.longitude+" Latitude:"+locjson.latitude)
+        
+    }
 })
-}
+
+weatherinfo.getWeatherInfo("Chennai",function(error,temperatureinfo){
+            if(error){
+                console.log(error)
+            }
+            else{
+                console.log("Current Temperature in Chennai:"+temperatureinfo)
+            }
+        
+        })
+
+
+//Test the following Case and observe the output.
+// CASE 1:Copy and Paste the above code to test with another valid city and observe the output.
+
+// CASE 2:Copy and Paste the above code to test with invalid city value and observe the output.
+
+
+
